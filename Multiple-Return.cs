bdb6dcca43050aeea08a40ee131801fbeabede4c
royngl22 <88992346+royngl22@users.noreply.github.com>
2021-09-11 21:54:17 +0700

    Update Multiple-Return.cs

--- Multiple-Return.cs
+++ Multiple-Return.cs
@@ -1,1 +1,22 @@
+using System;
 
+class Program
+{
+    public static void Main()
+    {
+        int Total;
+        int Product;
+
+        Calculate(10, 20, out Total, out Product);
+
+        Console.WriteLine("Total : {0} & Product : {1}", Total, Product);
+    }
+
+    public static void Calculate(int FN, int SN, out int Sum, out int Product)
+    {
+        Sum = FN + SN;
+        Product = FN * SN;
+    }
+
+
+}
