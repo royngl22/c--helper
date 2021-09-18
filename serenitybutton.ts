d4532cce88e8ff8d1a5556d2d4f2f6a20ddea5de
royngl22 <88992346+royngl22@users.noreply.github.com>
2021-09-18 23:46:35 +0700

    Create serenitybutton.ts

---
+++ serenitybutton.ts
@@ -0,0 +1,29 @@
+protected getButtons(): Serenity.ToolButton[] {
+            let buttons = super.getButtons();
+            //var btn = Q.first(buttons, x => x.cssClass == "tool-button save-and-close-button");
+            //btn.title = "Lock MRP";
+
+            let saveAndCloseBtn: Serenity.ToolButton = buttons[0];
+            saveAndCloseBtn.title = "Add New Site";
+
+            let applyButton: Serenity.ToolButton = buttons[1];
+            applyButton.visible = false;
+              
+            //applyButton.title = Q.text("TEST");
+            //applyButton.title = "TEST";
+            
+
+            let deleteBtn: Serenity.ToolButton = buttons[2];
+            //deleteBtn.title = Q.text("Site.MyDialogName.DeleteText"); // In Texts.cs
+            deleteBtn.title = "TEXT"
+
+            //Add a Button
+            buttons.push({
+                title: "Add New Site",
+                hint: "add new site",
+                cssClass: "tool-button save-and-close-button",
+                icon: "fa-hand-lizard-o"
+            })
+
+            return buttons;
+        }
